8d2f1de2723f8f2929a01dbb089923e92bad275d
maryemonty <113999089+maryemonty@users.noreply.github.com>
2023-03-17 20:10:51 +0100

    finalmenteeeeeee

--- backoffice.js
+++ backoffice.js
@@ -1,19 +1,50 @@
-const id = new URLSearchParams(window.location.search).get('_id')
 const apiURL = 'https://striveschool-api.herokuapp.com/api/product/'
-const a = {
-    headers: {
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDE0M2E0ZWY4MWI0MjAwMTM5YjI4MzgiLCJpYXQiOjE2NzkwNDcyNDYsImV4cCI6MTY4MDI1Njg0Nn0.JOuvgWEwI9jKGVynvQ-fMowvbLurAiPY0bRHB_9XZ-k"
-        }
-}
 
-const endpoint = id ? apiURL + id : apiURL
-const method = id ? "PUT" : "POST"
+const n = document.getElementById('name')
+const d = document.getElementById('description')
+const p = document.getElementById('price')
+const b = document.getElementById('brand')
 
-fetch(endpoint,a)
-.then(r => r.json())
-.then(body => {
-    for(let i = 0;i<body.length;i++){
-        const {brand, name, imageUrl, price, description} = body[i]
-        console.log(body);
+const form = document.getElementById('form')
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    const products = {
+        name: n.value,
+        description: d.value,
+        price: p.value,
+        brand: b.value,
+        imageUrl: "https://t3.ftcdn.net/jpg/05/53/96/26/360_F_553962649_kYCSSF6qJ9oR83QLnaBlwOgNj58kxhyB.jpg"
     }
+    
+    fetch(apiURL,{
+        method: 'POST',
+        headers: {
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDE0M2E0ZWY4MWI0MjAwMTM5YjI4MzgiLCJpYXQiOjE2NzkwNDcyNDYsImV4cCI6MTY4MDI1Njg0Nn0.JOuvgWEwI9jKGVynvQ-fMowvbLurAiPY0bRHB_9XZ-k",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(products)
+    })
 })
+
+
+
+
+// function submitted(event){
+//     event.preventDefault()
+//     fetch(apiURL,a)
+//     .then(r => r.json())
+//     .then(b => {
+//         const products = {
+//             name: document.getElementById('name').value,
+//             description: document.getElementById('description').value,
+//             price: document.getElementById('brand').value,
+//             brand: document.getElementById('brand').value,
+//             imageUrl: "https://t3.ftcdn.net/jpg/05/53/96/26/360_F_553962649_kYCSSF6qJ9oR83QLnaBlwOgNj58kxhyB.jpg"
+//         }
+//         b = JSON.stringify(products)
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
+// }
